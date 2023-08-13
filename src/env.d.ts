bcfbf50846e068ff94b0de0de077a723085a0000
JesusP2 <jesus_perez99@hotmail.com>
2023-08-13 11:02:09 -0600

    lucia + astro stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,38 @@
-/// <reference types="astro/client-image" />
-interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    // more env variables...
+///// <reference types="@astrojs/image/client" />
+/// <reference types="vite/client" />
+interface ImportMeta {
+  env: {
+    readonly ANIME_API: string;
+    readonly DATABASE_URL: string;
+    readonly MODE: string;
+    readonly BASE_URL: string;
+    readonly PROD: boolean;
+    readonly DEV: boolean;
+    readonly SSR: boolean;
+  };
 }
 
-interface ImportMeta {
-    readonly env: ImportMetaEnv
+interface ImportMetaEnv {
+  env: {
+    readonly ANIME_API: string;
+    readonly DATABASE_URL: string;
+    readonly MODE: string;
+    readonly BASE_URL: string;
+    readonly PROD: boolean;
+    readonly DEV: boolean;
+    readonly SSR: boolean;
+  };
+}
+declare namespace Lucia {
+  type Auth = import("./lib/db/lucia").Auth;
+  type DatabaseUserAttributes = {
+    username: string;
+  };
+  type DatabaseSessionAttributes = {};
+}
+
+declare namespace App {
+	interface Locals {
+		auth: import("lucia").AuthRequest;
+	}
 }
---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { auth } from "./lib/db/lucia";
+
+import type { MiddlewareResponseHandler } from "astro";
+
+export const onRequest: MiddlewareResponseHandler = async (context, next) => {
+  try {
+    context.locals.auth = auth.handleRequest(context);
+  } catch(err) {
+    console.error('no idea if this is even the problem', err)
+  }
+	return await next();
+};
+
