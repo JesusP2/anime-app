3237b5c4a8fdc1f068c503e904d1ea33112c008e
JesusP2 <jesus_perez99@hotmail.com>
2023-10-14 14:54:53 -0600

    fixed some broken styles, finished manga and character cards

--- src/components/solid/select-search-type.tsx
+++ src/components/solid/select-search-type.tsx
@@ -15,24 +15,25 @@ export function SelectSearchType(props: {
 }) {
   const [value, setValue] = createSignal(props.defaultValue || 'all');
   return (
-    <Select
-      name={props.name}
-      value={value()}
-      onChange={setValue}
-      placeholder="All"
-      options={props.options}
-      itemComponent={(props) => (
-        <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
-      )}
-    >
-      <SelectTrigger
-        class={clsx(
-          'w-32 rounded-l-md flex-none text-gray-500 h-10'
+    <>
+      <Select
+        name={props.name}
+        value={value()}
+        onChange={setValue}
+        placeholder="All"
+        options={props.options}
+        itemComponent={(props) => (
+          <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
         )}
       >
-        <SelectValue<string>>{(state) => state.selectedOption()}</SelectValue>
-      </SelectTrigger>
-      <SelectContent class={clsx("w-40 h-[9.5rem] bg-white")} />
-    </Select>
+        <SelectTrigger
+          class={clsx('w-32 rounded-l-md flex-none text-gray-500 h-10')}
+        >
+          <SelectValue<string>>{(state) => state.selectedOption()}</SelectValue>
+        </SelectTrigger>
+        <SelectContent class={clsx('w-40 h-[9.5rem] bg-white')} />
+      </Select>
+      <input name={props.name} hidden value={value()} />
+    </>
   );
 }
