f4e4ef473fa88af22521925685247a62e39a2a34
JesusP2 <jesus_perez99@hotmail.com>
2024-03-08 22:36:29 -0600

    upated property to entityType

--- apps/web/src/pages/api/anime.ts
+++ apps/web/src/pages/api/anime.ts
@@ -1,20 +1,9 @@
 import type { APIRoute } from 'astro';
-import { db } from '../../lib/db/pool';
-import {
-  anime,
-  entityActionsTracker,
-  manga,
-  trackedEntity,
-} from '@/lib/db/schema';
-import { and, eq } from 'drizzle-orm';
 import { z } from 'zod';
-import { randomUUID } from 'crypto';
 import { entityStatus, json } from '@/lib/utils';
-import { fetchEntityByMalId } from '@/lib/jikan';
-import { userTrackedEntityRepository } from '@/lib/db/repositories/user-tracked-entity.repository';
 import { parseCookie } from 'lucia/utils';
-import type { MangaFull, AnimeFull } from '@/lib/types';
 import { postSchema } from '@/lib/schemas';
+import { usersTrackedEntitiesRepository } from '@/lib/storage/user-tracked-entity';
 
 /**
  * @description
@@ -36,92 +25,10 @@ export const POST: APIRoute = async (context) => {
     return json({ message: 'Unauthorized' }, { status: 401 });
   }
   const userId = payload.data.userId;
-
-  let isEntityStoredInDb: boolean;
-  if (payload.data.entity === 'ANIME') {
-    isEntityStoredInDb = !!(
-      await db
-        .select()
-        .from(anime)
-        .where(eq(anime.mal_id, payload.data.malId))
-        .limit(1)
-    )[0];
-  } else {
-    isEntityStoredInDb = !!(
-      await db
-        .select()
-        .from(manga)
-        .where(eq(manga.mal_id, payload.data.malId))
-        .limit(1)
-    )[0];
-  }
-
-  let entityFetched: typeof payload.data.entity extends 'ANIME' ?
-   AnimeFull 
-  : MangaFull;
-  if (!isEntityStoredInDb) {
-    const _entityFetched = await fetchEntityByMalId(
-      payload.data.entity,
-      payload.data.malId,
-    );
-    if (!_entityFetched.success) {
-      return json(
-        { message: 'Could not fetch data succesfully' },
-        { status: 500 },
-      );
-    }
-    entityFetched = _entityFetched.data as typeof payload.data.entity extends (
-      'ANIME'
-    ) ?
-      AnimeFull
-    : MangaFull;
-  }
-
-  const isUserTrackingEntity = !!(
-    await db
-      .select()
-      .from(trackedEntity)
-      .where(
-        and(
-          eq(trackedEntity.userId, userId),
-          eq(trackedEntity.malId, payload.data.malId),
-        ),
-      )
-  )[0];
-  // await userTrackedEntityRepository.create({
-  //   payload: payload.data,
-  //   isUserTrackingEntity,
-  //   entityFetched,
-  //   userId,
-  //   isEntityStoredInDb,
-  // });
-  await db.transaction(async (tx) => {
-    const trackedEntityId = randomUUID();
-    if (!isUserTrackingEntity) {
-      await tx.insert(trackedEntity).values({
-        id: trackedEntityId,
-        userId: userId,
-        userType: payload.data.userType,
-        entityType: payload.data.entity,
-        entityStatus: payload.data.entityStatus,
-        malId: payload.data.malId,
-      });
-    } else {
-      await tx.update(trackedEntity).set({ entityType: payload.data.entity });
-    }
-
-    if (!isEntityStoredInDb) {
-      await tx
-        .insert(payload.data.entity === 'ANIME' ? anime : manga)
-        .values(entityFetched as any);
-    }
-
-    await tx.insert(entityActionsTracker).values({
-      trackedEntityId,
-      operation: payload.data.entityStatus,
-    });
+  await usersTrackedEntitiesRepository.createOrUpdateUsersEntity({
+    payload: payload.data,
+    userId,
   });
-
   return json({ message: 'completed' }, { status: 200 });
 };
 
@@ -145,7 +52,7 @@ export const GET: APIRoute = async (context) => {
   }
 
   return json(
-    await userTrackedEntityRepository.findEntities({
+    await usersTrackedEntitiesRepository.findUsersEntities({
       userId: payload.data.userId,
       entityType: payload.data.entityType,
       status: payload.data.entityStatus,
