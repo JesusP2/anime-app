10e2e0208055567fa30804e0d008ce696b9385f5
JesusP2 <jesus_perez99@hotmail.com>
2024-03-11 21:46:04 -0600

    got sidebar working

--- apps/web/src/components/react/sidebar.tsx
+++ apps/web/src/components/react/sidebar.tsx
@@ -1,14 +1,14 @@
 import { buttonVariants } from '@repo/shadcn/button';
 import { cn } from '@/lib/utils';
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { VscGroupByRefType } from 'react-icons/vsc';
-import { VscVmRunning } from 'react-icons/vsc';
-import { GoProject } from 'react-icons/go';
+import { IoPauseCircleOutline } from "react-icons/io5";
+import { PiTrash } from "react-icons/pi";
+import { GrPlan } from "react-icons/gr";
 import { FiChevronDown } from 'react-icons/fi';
-import { BsActivity } from 'react-icons/bs';
 import { BsWindowStack } from 'react-icons/bs';
-import { HiOutlineUsers } from 'react-icons/hi2';
+import { IoIosSearch } from "react-icons/io";
 import { GoGear } from 'react-icons/go';
-import { TiPin, TiPinOutline } from 'react-icons/ti';
 import {
   Collapsible,
   CollapsibleContent,
@@ -16,16 +16,13 @@ import {
 } from '@repo/shadcn/collapsible';
 import { UserDropdown } from '@/components/react/user-dropdown';
 import { SidebarLink } from './sidebar-link';
-import { useState } from 'react';
-// import { bungeeFont } from '@/lib/fonts';
 
 export function Sidebar({ pathname }: { pathname: string }) {
-  const [isSidebarPinned, setPinSidebar] = useState(false);
 
   return (
     <aside
       className={cn(
-        'bg-white rounded-md duration-200 h-screen overflow-hidden p-4 px-2 text-black flex flex-col justify-between group shadow-lg shadow-neutral-400 sidebar',
+        'bg-white rounded-md delay-100 duration-200 h-screen overflow-hidden p-4 px-2 text-black flex flex-col justify-between group shadow-lg shadow-neutral-400 sidebar',
       )}
     >
       <div>
@@ -33,35 +30,28 @@ export function Sidebar({ pathname }: { pathname: string }) {
           <div className="flex gap-x-0">
             <span
               className={cn(
-                'text-3xl font-bold text-left ml-3 mb-4',
-                // bungeeFont.className,
+                'text-3xl font-bold text-left ml-3 mb-4 font-bungee',
               )}
             >
               G
             </span>
             <span
               className={cn(
-                'text-3xl font-bold text-left w-full mb-4 whitespace-nowrap duration-300 sidebar-text',
-                // bungeeFont.className,
+                'text-3xl font-bold text-left w-full mb-4 whitespace-nowrap duration-200 sidebar-text delay-200 font-bungee',
               )}
             >
               aphite
             </span>
           </div>
-          <button onClick={() => setPinSidebar((prev) => !prev)}>
-            {isSidebarPinned ?
-              <TiPin size={20} className="mt-1" />
-            : <TiPinOutline size={20} className="mt-1" />}
-          </button>
         </div>
         <div className="flex flex-col gap-y-2">
           <SidebarLink pathname={pathname} href="/search">
-            <GoProject size={22} className="min-w-[22px]" />
-            <span className="sidebar-text">Search</span>
+            <IoIosSearch size={22} className="min-w-[22px]" />
+            <span className="sidebar-text delay-200">Search</span>
           </SidebarLink>
           <SidebarLink pathname={pathname} href="/current-season">
             <BsWindowStack size={20} className="min-w-[20px]" />
-            <span className="sidebar-text">Current season</span>
+            <span className="sidebar-text delay-200">Current season</span>
           </SidebarLink>
           <Collapsible>
             <CollapsibleTrigger
@@ -75,33 +65,33 @@ export function Sidebar({ pathname }: { pathname: string }) {
                   size={20}
                   className="min-w-[20px] min-h-[20px]"
                 />
-                <span className="sidebar-text">Animes</span>
+                <span className="sidebar-text delay-200">Animes</span>
               </span>
               <FiChevronDown
                 size={18}
-                className={cn('rotate-180 sidebar-text')}
+                className={cn('rotate-180 sidebar-text delay-200')}
               />
             </CollapsibleTrigger>
             <CollapsibleContent
               className={cn(
-                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text',
+                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text delay-200',
               )}
             >
               <SidebarLink pathname={pathname} href="/animes/completed">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Completed</span>
+                <IoMdCheckmarkCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Completed</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/animes/planning-to-watch">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Planning to watch</span>
+                <GrPlan size={16} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Planning to watch</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/animes/dropped">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Dropped</span>
+                <PiTrash size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Dropped</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/animes/on-hold">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Paused</span>
+                <IoPauseCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Paused</span>
               </SidebarLink>
             </CollapsibleContent>
           </Collapsible>
@@ -117,33 +107,33 @@ export function Sidebar({ pathname }: { pathname: string }) {
                   size={20}
                   className="min-w-[20px] min-h-[20px]"
                 />
-                <span className="sidebar-text">Mangas</span>
+                <span className="sidebar-text delay-200">Mangas</span>
               </span>
               <FiChevronDown
                 size={18}
-                className={cn('rotate-180 sidebar-text')}
+                className={cn('rotate-180 sidebar-text delay-200')}
               />
             </CollapsibleTrigger>
             <CollapsibleContent
               className={cn(
-                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text',
+                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text delay-200',
               )}
             >
               <SidebarLink pathname={pathname} href="/mangas/completed">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Completed</span>
+                <IoMdCheckmarkCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Completed</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/mangas/planning-to-watch">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Planning to watch</span>
+                <GrPlan size={16} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Planning to watch</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/mangas/dropped">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Dropped</span>
+                <PiTrash size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Dropped</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/mangas/on-hold">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Paused</span>
+                <IoPauseCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Paused</span>
               </SidebarLink>
             </CollapsibleContent>
           </Collapsible>
@@ -159,33 +149,33 @@ export function Sidebar({ pathname }: { pathname: string }) {
                   size={20}
                   className="min-w-[20px] min-h-[20px]"
                 />
-                <span className="sidebar-text">Light Novels</span>
+                <span className="sidebar-text delay-200">Light Novels</span>
               </span>
               <FiChevronDown
                 size={18}
-                className={cn('rotate-180 sidebar-text')}
+                className={cn('rotate-180 sidebar-text delay-200')}
               />
             </CollapsibleTrigger>
             <CollapsibleContent
               className={cn(
-                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text',
+                'border-l-[2px] border-slate-200 pl-4 ml-[1.3rem] sidebar-text duration-1000',
               )}
             >
               <SidebarLink pathname={pathname} href="/light-novles/completed">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Completed</span>
+                <IoMdCheckmarkCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Completed</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/light-novels/planning-to-watch">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Planning to watch</span>
+                <GrPlan size={16} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Planning to watch</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/light-novels/dropped">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Dropped</span>
+                <PiTrash size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Dropped</span>
               </SidebarLink>
               <SidebarLink pathname={pathname} href="/light-novels/on-hold">
-                <VscGroupByRefType size={20} className="min-w-[20px]" />
-                <span className="sidebar-text">Paused</span>
+                <IoPauseCircleOutline size={20} className="min-w-[20px]" />
+                <span className="sidebar-text delay-200">Paused</span>
               </SidebarLink>
             </CollapsibleContent>
           </Collapsible>
@@ -193,7 +183,7 @@ export function Sidebar({ pathname }: { pathname: string }) {
         <div className="border-t border-dashed border-stone-300 mt-4 pt-4">
           <SidebarLink pathname="1" href={`/organization/1/overview`}>
             <GoGear size={20} className="min-w-[20px]" />
-            <span className="sidebar-text">Settings</span>
+            <span className="sidebar-text delay-200">Settings</span>
           </SidebarLink>
         </div>
       </div>
